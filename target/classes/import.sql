ccd17ab703171b73c0427911f5c6da2a43562033
ugflosant@gmail.com <ugflosant@gmail.com>
2024-08-28 00:19:02 -0600

    Inicio git, cambios hasta maker servoces autowired falla

---
+++ target/classes/import.sql
@@ -0,0 +1,24 @@
+INSERT INTO fabricante (id, nombre) VALUES (1, 'AMD');
+INSERT INTO fabricante (id, nombre) VALUES (2, 'NVIDIA');
+INSERT INTO fabricante (id, nombre) VALUES (3, 'MSI');
+INSERT INTO fabricante (id, nombre) VALUES (4, 'Corsair');
+INSERT INTO fabricante (id, nombre) VALUES (5, 'Samsung');
+INSERT INTO fabricante (id, nombre) VALUES (6, 'NZXT');
+INSERT INTO fabricante (id, nombre) VALUES (7, 'ASUS');
+INSERT INTO fabricante (id, nombre) VALUES (8, 'Razer');
+INSERT INTO fabricante (id, nombre) VALUES (9, 'Logitech');
+INSERT INTO fabricante (id, nombre) VALUES (10, 'HyperX');
+
+
+INSERT INTO producto (id, nombre, precio, id_fabricante) VALUES (1, 'Procesador AMD Ryzen 7 5800X', 399.99, 1);
+INSERT INTO producto (id, nombre, precio, id_fabricante) VALUES (2, 'Tarjeta Gráfica NVIDIA GeForce RTX 3080', 799.99, 2);
+INSERT INTO producto (id, nombre, precio, id_fabricante) VALUES (3, 'Placa Base MSI B550 Gaming Plus', 149.99, 3);
+INSERT INTO producto (id, nombre, precio, id_fabricante) VALUES (4, 'Memoria RAM Corsair Vengeance 16GB DDR4', 89.99, 4);
+INSERT INTO producto (id, nombre, precio, id_fabricante) VALUES (5, 'Disco Duro SSD Samsung 970 EVO Plus 1TB', 139.99, 5);
+INSERT INTO producto (id, nombre, precio, id_fabricante) VALUES (6, 'Fuente de Alimentación Corsair RM850x 850W', 129.99, 4);
+INSERT INTO producto (id, nombre, precio, id_fabricante) VALUES (7, 'Caja NZXT H510 Elite', 109.99, 6);
+INSERT INTO producto (id, nombre, precio, id_fabricante) VALUES (8, 'Sistema de Refrigeración Líquida NZXT Kraken X63', 149.99, 6);
+INSERT INTO producto (id, nombre, precio, id_fabricante) VALUES (9, 'Monitor Gaming ASUS ROG Swift 27" 144Hz', 499.99, 7);
+INSERT INTO producto (id, nombre, precio, id_fabricante) VALUES (10, 'Teclado Mecánico Razer BlackWidow Elite', 129.99, 8);
+INSERT INTO producto (id, nombre, precio, id_fabricante) VALUES (11, 'Mouse Gaming Logitech G502 HERO', 49.99, 9);
+INSERT INTO producto (id, nombre, precio, id_fabricante) VALUES (12, 'Auriculares Gaming HyperX Cloud II', 99.99, 10);
